bfb6a1860305c3c9276a8e4b8ad9ee30c61ef72e
chanicoh <chanicoh@g.jct.ac.il>
2026-02-01 12:54:26 +0200

    drone and station

--- server/routers/Drone.js
+++ server/routers/Drone.js
@@ -1,18 +1,18 @@
 import express from "express";
-import {
-  createDrone,
-  getDrones,
-  getDroneById,
-  updateDrone,
-  deleteDrone
-} from "../controllers/drone.controller.js";
+import * as controller from "../controllers/Drone.js";
 
 const router = express.Router();
 
-router.post("/", createDrone);
-router.get("/", getDrones);
-router.get("/:id", getDroneById);
-router.put("/:id", updateDrone);
-router.delete("/:id", deleteDrone);
+router.post("/", controller.createDrone);
+router.get("/", controller.getDrones);
+router.get("/available", controller.getAvailableDrones);
+router.get("/:id", controller.getDroneById);
+router.put("/:id", controller.updateDrone);
+router.delete("/:id", controller.deleteDrone);
+
+router.patch("/:id/assign-station", controller.assignDroneToStation);
+router.patch("/:id/charging", controller.sendToCharging);
+router.patch("/:id/maintenance", controller.sendToMaintenance);
+router.patch("/:id/available", controller.setAvailable);
 
 export default router;
