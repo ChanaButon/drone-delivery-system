3851bc44c2d3dd53d2fc47988fcfd0df762c70fa
ChanaButon <chanabuton@gmail.com>
2026-02-11 19:28:54 +0200

    fix delivery

--- server/services/Delivery.js
+++ server/services/Delivery.js
@@ -1,41 +1,30 @@
 import { Delivery } from "../models/Delivery.js";
 
-/**
- * יצירת משלוח חדש
- */
 export const createDeliveryService = async (deliveryData) => {
   const delivery = new Delivery(deliveryData);
   return await delivery.save();
 };
 
-/**
- * שליפת כל המשלוחים
- */
+
 export const getAllDeliveriesService = async () => {
   return await Delivery.find()
     .populate("senderId receiverId droneId stationId");
 };
 
-/**
- * שליפת משלוח לפי ID
- */
+
 export const getDeliveryByIdService = async (id) => {
   return await Delivery.findById(id)
     .populate("senderId receiverId droneId stationId");
 };
 
-/**
- * משלוחים של לקוח (שולח או מקבל)
- */
+
 export const getDeliveriesByUserService = async (userId) => {
   return await Delivery.find({
     $or: [{ senderId: userId }, { receiverId: userId }]
   });
 };
 
-/**
- * שיבוץ רחפן למשלוח
- */
+
 export const assignDroneService = async (deliveryId, droneId) => {
   return await Delivery.findByIdAndUpdate(
     deliveryId,
@@ -48,9 +37,6 @@ export const assignDroneService = async (deliveryId, droneId) => {
   );
 };
 
-/**
- * עדכון סטטוס משלוח
- */
 export const updateDeliveryStatusService = async (deliveryId, status) => {
   return await Delivery.findByIdAndUpdate(
     deliveryId,
